3a91544c37ea5e0d75fad455e8c7d427a5a83c05
Nyasha Hama <nyashahama45@gmail.com>
2026-04-23 06:07:34 +0200

    correctly configured for tailwindcss v4

--- apps/portal/postcss.config.mjs
+++ apps/portal/postcss.config.mjs
@@ -1,11 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('postcss-load-config').Config} */
 export default {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
+  plugins: {
+    '@tailwindcss/postcss': {},
   },
-  plugins: [],
 };
