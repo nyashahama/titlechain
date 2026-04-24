a790945a64489ef3cd1fdaf9f1132c7c05848fa3
Nyasha Hama <nyashahama45@gmail.com>
2026-04-24 06:21:43 +0200

    feat: add landing page animations and scroll reveals

--- apps/portal/app/test-setup.ts
+++ apps/portal/app/test-setup.ts
@@ -8,6 +8,14 @@ class ResizeObserverMock {
 
 global.ResizeObserver = ResizeObserverMock as unknown as typeof ResizeObserver;
 
+class IntersectionObserverMock {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+global.IntersectionObserver = IntersectionObserverMock as unknown as typeof IntersectionObserver;
+
 if (!Element.prototype.scrollIntoView) {
   Element.prototype.scrollIntoView = () => {};
 }
